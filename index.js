4f3f42125b624894e9cb9d9c41ba36453778ea82
Murat Eker <murateker9166@gmail.com>
2024-08-04 19:51:39 +0300

    helloworld

---
+++ index.js
@@ -0,0 +1,28 @@
+const { NinjaAPI, WatchAPI } = require("poe-api-manager");
+
+// NinjaAPI ve WatchAPI nesnelerini başlatın
+const ninjaAPI = new NinjaAPI("League-Name");
+const watchAPI = new WatchAPI("League-Name");
+
+const requestedProperties = ["id", "name", "icon"];
+
+const fetchData = async () => {
+  try {
+    // poe.ninja'dan Yağ (Oil) verilerini al
+    const oilData = await ninjaAPI.itemView.oil.getData(requestedProperties);
+    console.log("poe.ninja Oil Data:", oilData);
+
+    // poe.ninja'dan Para Birimi (Currency) verilerini al
+    const currencyData = await ninjaAPI.currencyView.currency.getData(requestedProperties);
+    console.log("poe.ninja Currency Data:", currencyData);
+
+    // poe.watch'tan Scarab verilerini al
+    const scarabData = await watchAPI.view.scarab.getData(requestedProperties);
+    console.log("poe.watch Scarab Data:", scarabData);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+// Verileri al ve göster
+fetchData();
