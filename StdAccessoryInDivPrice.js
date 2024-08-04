2911762408a59d98116f27ab74797b2d908a682e
Murat Eker <murateker9166@gmail.com>
2024-08-04 21:31:06 +0300

    added Std league

---
+++ StdAccessoryInDivPrice.js
@@ -0,0 +1,22 @@
+const { NinjaAPI, WatchAPI } = require("poe-api-manager");
+const ninjaAPI = new NinjaAPI("Standard"); // League name
+const watchAPI = new WatchAPI("Standard");
+
+const requestedProperties = ["id", "name", "divineValue", "explicitModifiers"];
+
+const iname = "Mageblood"; // case sensitive item name !
+
+const fetchItemPrice = async () => {
+
+  const theItem = await ninjaAPI.itemView.uniqueAccessory.getData(requestedProperties);
+  const itemOut = theItem.find(item => item.name === iname);
+  
+  if (itemOut) {
+    console.log("poe.ninja Currency Data:", itemOut);
+  } else {
+    console.log(iname, "not found.");
+  }
+  
+};
+
+fetchItemPrice();
