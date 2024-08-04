c8900307daa55213935a27d3d63c3ec01ad8f4dd
Murat Eker <murateker9166@gmail.com>
2024-08-04 20:26:10 +0300

    even more testing

--- headhunter.js
+++ headhunter.js
@@ -4,15 +4,11 @@ const ninjaAPI = new NinjaAPI("Settlers");
 
 const fetchHeadhunterPrice = async () => {
   try {
-    // Unique Accessories verilerini al
     const uniqueAccessories = await ninjaAPI.itemView.uniqueAccessory.getData();
-    
-    // "Headhunter" item'ini bul
     const headhunter = uniqueAccessories.find(item => item.name === "Headhunter");
 
     if (headhunter) {
-      console.log(`Headhunter item found:`, headhunter);
-      console.log(`Headhunter Price in Chaos:`, headhunter.chaosEquivalent);
+      console.log(`Headhunter Price in Divine :`, headhunter.divineValue);
     } else {
       console.log("Headhunter item not found.");
     }
@@ -21,5 +17,4 @@ const fetchHeadhunterPrice = async () => {
   }
 };
 
-// Headhunter itemini arama
 fetchHeadhunterPrice();
