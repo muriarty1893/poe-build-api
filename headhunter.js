6bf9c31d87dc34cf8fbc5cb6d14d9a3fd86dcaf0
Murat Eker <murateker9166@gmail.com>
2024-08-04 20:30:18 +0300

    configs

--- headhunter.js
+++ headhunter.js
@@ -2,18 +2,20 @@ const { NinjaAPI } = require("poe-api-manager");
 
 const ninjaAPI = new NinjaAPI("Settlers");
 
+const iname = "headhunter";
+
 const fetchHeadhunterPrice = async () => {
   try {
     const uniqueAccessories = await ninjaAPI.itemView.uniqueAccessory.getData();
     const headhunter = uniqueAccessories.find(item => item.name === "Headhunter");
 
     if (headhunter) {
-      console.log(`Headhunter Price in Divine :`, headhunter.divineValue);
+      console.log(iname ,`Price in Divine :`, headhunter.divineValue);
     } else {
-      console.log("Headhunter item not found.");
+      console.log(iname, "item not found.");
     }
   } catch (error) {
-    console.error("Error fetching Headhunter data:", error);
+    console.error("Error fetching", iname, "data:", error);
   }
 };
 
